6c30d7ba3f38a82c4d7beb30648a6ab6225bc631
Cam <zawacki.cameron@gmail.com>
2014-07-10 22:26:02 -0400

    Everything

---
+++ 2D2PlayerCTF/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+    private const string roomName = "Test Room";
+    private RoomInfo[] roomsList;
+    public GameObject playerPrefab;
+	public string input = "Enter Room Name Here";
+	private bool createServer = false;
+	private bool joinRoom = false;
+
+
+	// Use this for initialization
+	void Start (){
+		PhotonNetwork.sendRate = 66; //Play with this and navmeshs
+		PhotonNetwork.sendRateOnSerialize = 66;
+        PhotonNetwork.ConnectUsingSettings("0.1"); //Version number of the game
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+  	void OnGUI(){
+
+
+		
+		float textFieldWidth = 200f;
+		float textFieldHeight = 20f;
+
+   	 	if (!PhotonNetwork.connected) {
+        	GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+   		} else if (PhotonNetwork.room == null){
+
+			if(!createServer && !joinRoom){
+        	// Create Room
+				if (GUI.Button(new Rect(Screen.width/2 - textFieldWidth/2, Screen.height/3 + textFieldHeight, textFieldWidth, 50), "Start Game"))
+					joinRoom = true;
+				//PhotonNetwork.CreateRoom(input, true, true, 4);
+ 
+				if (GUI.Button(new Rect(Screen.width/2 - textFieldWidth/2, Screen.height/3 + textFieldHeight + 60, textFieldWidth, 50), "Start Server"))
+					createServer = true;
+			} else if(joinRoom){
+				input = GUI.TextField (new Rect (Screen.width/2 - textFieldWidth/2, Screen.height/3 - textFieldHeight, textFieldWidth, 20), input, 25);
+				if (GUI.Button(new Rect(Screen.width/2 - textFieldWidth/2, Screen.height/3 + textFieldHeight, textFieldWidth, 50), "Join Room"))
+				
+				for (int i = 0; i < roomsList.Length; i++){
+					if (input.Equals(roomsList[i].name))
+						PhotonNetwork.JoinRoom(input);
+				}
+
+				if (GUI.Button(new Rect(Screen.width/2 - textFieldWidth/2, Screen.height/3 + textFieldHeight + 60, textFieldWidth, 50), "Back"))
+					joinRoom = false;
+			} else if(createServer){
+				input = GUI.TextField (new Rect (Screen.width/2 - textFieldWidth/2, Screen.height/3 - textFieldHeight, textFieldWidth, 20), input, 25);
+				if (GUI.Button(new Rect(Screen.width/2 - textFieldWidth/2, Screen.height/3 + textFieldHeight, textFieldWidth, 50), "Create Server"))
+					PhotonNetwork.CreateRoom(input, true, true, 4);
+				
+				if (GUI.Button(new Rect(Screen.width/2 - textFieldWidth/2, Screen.height/3 + textFieldHeight + 60, textFieldWidth, 50), "Back"))
+					createServer = false;
+			}
+
+
+		}
+	}
+
+    void OnReceivedRoomListUpdate(){
+        roomsList = PhotonNetwork.GetRoomList();
+    }
+
+    void OnJoinedRoom() {
+        Debug.Log("Connected to Room");
+        //Spawn Player
+        PhotonNetwork.Instantiate(playerPrefab.name,Vector2.zero * 5, Quaternion.identity,0);
+    }
+
+}
---
+++ 2D2PlayerCTF/Assets/Scripts/Player.cs
@@ -0,0 +1,211 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Photon.MonoBehaviour {
+
+	float move;
+	public float maxSpeed = 2f;
+	public float crawlSpeed = 1f;
+	public float walkSpeed = 2f;
+	public float runSpeed = 6f;
+	private bool facingRight = true;
+	public bool dashing = false;
+	public float dashTimer = 0.5f;
+	private float dashCounter = 0f;
+
+	private float otherMove;
+	private float otherVSpeed;
+	private bool otherGrounded; //Condense into 0101010101
+	private bool otherCrouch;
+	private bool otherDash;
+
+	Animator anim;
+
+	bool grounded = false;
+	public Transform groundCheck;
+	float goundRadius = 0.1f;
+	public LayerMask whatIsGround;
+	public float jumpForce = 700f;
+
+	public bool crouching = false;
+
+    private float lastSynchronizationTime = 0f;
+	private float syncDelay = 0f;
+	private float syncTime = 0f;
+	private Vector3 syncStartPosition = Vector3.zero;
+	private Vector3 syncEndPosition = Vector3.zero;
+
+
+
+	void Start(){
+		anim = GetComponent<Animator>();
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        if(photonView.isMine){
+       		InputMovement();
+			InputColorChange();
+		} else {
+			SyncedMovement();
+		}
+	}
+
+	void Update(){
+
+		if(dashing && (Input.GetKeyUp(KeyCode.D)||Input.GetKeyUp(KeyCode.A)) && dashTimer < 0 ){
+			dashing = false;
+		}
+
+		if(Input.GetKeyDown(KeyCode.D)||Input.GetKeyDown(KeyCode.A) && grounded && !crouching){
+			if ( dashTimer > 0 && dashCounter == 1/*Number of Taps you want Minus One*/){
+				dashing = true;
+			}else{
+				dashTimer = 0.5f ; 
+				dashCounter += 1 ;
+			}
+		}
+		if ( dashTimer > 0 ){
+			
+			dashTimer -= 1 * Time.deltaTime ;
+			
+		}else{
+			dashCounter = 0 ;
+		}
+	
+		
+		if(grounded && Input.GetAxis("Jump") > 0.01){ 
+			Vector3 v = rigidbody2D.velocity;
+			if(dashing)
+				v.y = 15f;
+			else
+				v.y = 10f;
+			rigidbody2D.velocity = v;
+			anim.SetBool("Ground",false);
+			
+		} 
+		
+		if(grounded && Input.GetAxis("Jump") < 0){
+			crouching = true;
+			dashing = false;
+		} else {
+			crouching = false;
+		}
+
+
+
+
+	}
+
+	private void InputColorChange(){
+		if(Input.GetKeyDown(KeyCode.R)){
+			ChangeColorTo(new Vector3(Random.Range(0f,1f),Random.Range(0f,1f),Random.Range(0f,1f)));
+		}
+		
+		
+	}
+
+    void InputMovement(){
+
+
+
+		if(dashing){
+			maxSpeed = runSpeed;
+		} else
+			maxSpeed = walkSpeed;
+		if(crouching){
+			maxSpeed = crawlSpeed;
+		}
+
+		move = Input.GetAxis("Horizontal");
+		grounded = Physics2D.OverlapCircle(groundCheck.position, goundRadius, whatIsGround);
+
+		anim.SetFloat("vSpeed",rigidbody2D.velocity.y);
+		anim.SetBool("Crouch",crouching);
+		anim.SetFloat("Speed", Mathf.Abs(move));
+		anim.SetBool("Dashing",dashing);
+		anim.SetBool ("Ground",grounded);
+
+		rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+
+		if(move > 0 && !facingRight){
+			flip();
+		} else if(move < 0 && facingRight){
+			flip();
+		}
+
+    }
+	
+	void flip(){
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+
+
+	private void SyncedMovement(){
+
+		if(otherMove > 0 && !facingRight){
+			flip();
+		} else if(otherMove < 0 && facingRight){
+			flip();
+		}
+
+		anim.SetFloat("Speed", Mathf.Abs(otherMove));
+		anim.SetBool ("Ground",otherGrounded);
+		anim.SetFloat("vSpeed",otherVSpeed);
+		anim.SetBool("Crouch",otherCrouch);
+		anim.SetBool("Dashing",otherDash);
+		syncTime += Time.deltaTime;
+		transform.position = Vector3.Lerp(syncStartPosition,syncEndPosition,syncTime/syncDelay);
+	}
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+
+        if (stream.isWriting){
+            stream.SendNext(transform.position);
+			stream.SendNext(rigidbody2D.velocity);
+			stream.SendNext(move);
+			stream.SendNext(grounded);
+			stream.SendNext(crouching);
+			stream.SendNext(dashing);
+			 
+		}else {
+
+			//syncEndPosition = (Vector3)stream.ReceiveNext();
+
+			Vector3 syncPosition = (Vector3)stream.ReceiveNext();
+			Vector2 syncVelocity = (Vector2)stream.ReceiveNext();
+			otherMove = (float)stream.ReceiveNext();
+			otherGrounded = (bool)stream.ReceiveNext();
+			otherCrouch = (bool)stream.ReceiveNext();
+			otherDash = (bool)stream.ReceiveNext();
+
+			otherVSpeed = syncVelocity.y;
+
+			syncTime = 0f;
+			syncDelay = Time.time - lastSynchronizationTime;
+			lastSynchronizationTime = Time.time;
+
+			syncEndPosition.x = syncPosition.x + syncVelocity.x * syncDelay;
+			syncEndPosition.y = syncPosition.y + syncVelocity.y * syncDelay;
+			//syncStartPosition = rigidbody2D.position;
+
+			syncStartPosition = transform.position;
+
+		}
+			
+    }
+
+	
+	[RPC] void ChangeColorTo(Vector3 color){
+		renderer.material.color = new Color(color.x,color.y,color.z,1f);
+		if(photonView.isMine){
+			photonView.RPC ("ChangeColorTo",PhotonTargets.OthersBuffered,color);
+		}
+	}
+
+}
